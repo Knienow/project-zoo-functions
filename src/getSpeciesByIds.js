134ea3da39affaaf0e9fade0d7cc116a7a3063e4
Katia Nienow <nienowkatia@gmail.com>
2023-02-01 19:18:53 -0300

    solução requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,11 +1,15 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (...specieId) => {
-  const result = data.species.filter((specie) => specie.id === specieId);
-  if (result !== undefined) {
-    return result;
+const getSpeciesByIds = (...ids) => {
+  if (ids.length <= 0) {
+    return [];
   }
-  return [];
+  const specieId = data.species.filter((id) => ids.includes(id.id));
+  // forEach
+  // const getId = data.species.find((specie) => specie.id === )
+  return specieId;
 };
+
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
