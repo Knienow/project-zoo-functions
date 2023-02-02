c8299eaf8176f93054f5228809c542ec965f98fe
Katia Nienow <nienowkatia@gmail.com>
2023-02-02 16:59:07 -0300

    solução requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
-const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+const getAnimalsOlderThan = (animalName, age) => {
+  const dataAnimal = data.species.filter((specie) => animalName === specie.name);
+  if (dataAnimal.length === 0) {
+    return false;
+  }
+  for (let index = 0; index < dataAnimal.length; index += 1) {
+    if (!dataAnimal[index].residents.every((elem) => elem.age >= age)) {
+      return false;
+    }
+  }
+  return true;
 };
 
+console.log(getAnimalsOlderThan('otters', 7));
 module.exports = getAnimalsOlderThan;
