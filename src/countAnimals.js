228706813f63f18c8065163e46a212f65a978fa6
Katia Nienow <nienowkatia@gmail.com>
2023-02-07 23:37:27 -0300

    solução requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,10 +1,36 @@
 const data = require('../data/zoo_data');
 
-const countAnimals = ({ species: animal }) => {
-  if ({ species: animal } === undefined) {
-    return data.species.length;
-  }
+const specieFilter = (filters) => data.species.filter((elem) => elem.name === filters.species);
+
+const filterByName = (specieFiltered) => specieFiltered[0].residents.length;
+
+const filterBySex = (filters, specieFiltered) => {
+  const filterSex = specieFiltered[0].residents.filter((elem) => elem.sex === filters.sex);
+  return filterSex.length;
 };
 
-console.log(countAnimals());
+const applyFilter = (filters) => {
+  const specieFiltered = specieFilter(filters);
+  if (specieFiltered.length === 0) {
+    return 0;
+  }
+  if (!filters.sex) {
+    return filterByName(specieFiltered);
+  }
+
+  return filterBySex(filters, specieFiltered);
+};
+
+const countAnimals = (filters) => {
+  if (filters && filters.species) {
+    return applyFilter(filters);
+  }
+
+  const object = {};
+  data.species.forEach((specie) => {
+    object[specie.name] = specie.residents.length;
+  });
+  return object;
+};
+
 module.exports = countAnimals;
