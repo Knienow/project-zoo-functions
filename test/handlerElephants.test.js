ac4fb158fa9ab8e4daf91a7510450f48572e69b0
Katia Nienow <nienowkatia@gmail.com>
2023-02-11 15:01:34 -0300

    solução requisito 7

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -16,4 +16,24 @@ describe('Testes da função HandlerElephants', () => {
     const expected = 10.5;
     expect(actual).toBeCloseTo(expected);
   });
+  it('ao passar o argumento location deve retornar a localização dos elefantes dentro do Zoológico', () => {
+    const actual = handlerElephants('location');
+    const expected = 'NW';
+    expect(actual).toEqual(expected);
+  });
+  it('ao passar o argumento popularity deve retornar a popularidade dos elefantes, equivalente a um número igual ou maior a 5', () => {
+    const actual = handlerElephants('popularity');
+    const expected = 5;
+    expect(actual).toBeGreaterThanOrEqual(expected);
+  });
+  it('ao passar o argumento availability deve retornar um array de dias da semana que não contém Monday', () => {
+    const actual = handlerElephants('availability');
+    const expected = ['Friday', 'Saturday', 'Sunday', 'Tuesday'];
+    expect(actual).toEqual(expected);
+  });
+  it('Não passando argumentos a função deve retornar undefined', () => {
+    const actual = handlerElephants();
+    const expected = undefined;
+    expect(actual).toEqual(expected);
+  });
 });
