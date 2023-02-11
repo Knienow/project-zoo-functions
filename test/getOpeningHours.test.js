aca3248b841a50fdd0c03b0bac9fb2980337b7e6
Katia Nienow <nienowkatia@gmail.com>
2023-02-11 15:17:44 -0300

    solução requisito 12

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,27 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('sem parâmetros a função deve retornar um objeto com os dias da semana e seus respectivos horários de funcionamento', () => {
+    const actual = getOpeningHours();
+    const expected = {
+      Tuesday: { open: 8, close: 6 },
+      Wednesday: { open: 8, close: 6 },
+      Thursday: { open: 10, close: 8 },
+      Friday: { open: 10, close: 8 },
+      Saturday: { open: 8, close: 10 },
+      Sunday: { open: 8, close: 8 },
+      Monday: { open: 0, close: 0 },
+    };
+    expect(actual).toEqual(expected);
+  });
+  it('ao passar os argumentos Monday e 09:00-AM deve retornar a string "The zoo is closed".', () => {
+    const actual = getOpeningHours('Monday', '09:00-AM');
+    const expected = 'The zoo is closed';
+    expect(actual).toEqual(expected);
+  });
+  it('ao passar os argumentos os argumentos Tuesday e 09:00-AM deve retornar a string "The zoo is open".', () => {
+    const actual = getOpeningHours('Tuesday', '09:00-AM');
+    const expected = 'The zoo is open';
+    expect(actual).toEqual(expected);
+  });
+});
