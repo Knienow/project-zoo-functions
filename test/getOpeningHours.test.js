9349c545a4c636e5bcc79e1d855db8df71ac8909
Katia Nienow <nienowkatia@gmail.com>
2023-02-11 15:33:17 -0300

    solução requisito 13 e refatoração do requisito 12

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -15,13 +15,24 @@ describe('Testes da função getOpeningHours', () => {
     expect(actual).toEqual(expected);
   });
   it('ao passar os argumentos Monday e 09:00-AM deve retornar a string "The zoo is closed".', () => {
-    const actual = getOpeningHours('Monday', '09:00-AM');
-    const expected = 'The zoo is closed';
-    expect(actual).toEqual(expected);
+    expect(getOpeningHours('Monday', '09:00-AM')).toEqual('The zoo is closed');
   });
   it('ao passar os argumentos os argumentos Tuesday e 09:00-AM deve retornar a string "The zoo is open".', () => {
-    const actual = getOpeningHours('Tuesday', '09:00-AM');
-    const expected = 'The zoo is open';
-    expect(actual).toEqual(expected);
+    expect(getOpeningHours('Tuesday', '09:00-AM')).toEqual('The zoo is open');
+  });
+  it('ao passar os argumentos os argumentos Wednesday e 09:00-PM deve retornar a string "The zoo is closed".', () => {
+    expect(getOpeningHours('Wednesday', '09:00-PM')).toEqual('The zoo is closed');
+  });
+  it('ao passar os argumentos Thu e 09:00-AM deve lançar uma exceção com a mensagem de erro', () => {
+    expect(getOpeningHours('Thu', '09:00-AM')).toThrow('The day must be valid. Example: Monday');
+  });
+  it('ao passar os argumentos Friday e 09:00-ZM deve lançar uma exceção com a mensagem de erro', () => {
+    expect(getOpeningHours('Friday', '09:00-ZM')).toThrow('The abbreviation must be \'AM\' or \'PM\'');
+  });
+  it('ao passar os argumentos Saturday e C9:00-AM deve lançar uma exceção com a mensagem de erro', () => {
+    expect(getOpeningHours('Saturday', 'C9:00-AM')).toThrow('The hour should represent a number');
+  });
+  it('ao passar os argumentos Sunday e 09:c0-AM deve lançar uma exceção com a mensagem de erro', () => {
+    expect(getOpeningHours('Sunday', '09:c0-AM')).toThrow('The minutes should represent a number');
   });
 });
