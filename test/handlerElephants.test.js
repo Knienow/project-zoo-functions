f74759c43a5bd27b5caff0ea174fcc603aa065ec
Katia Nienow <nienowkatia@gmail.com>
2023-02-12 15:25:20 -0300

    solução requisito 15 e refatoração dos testes da função handlerElephants

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -2,9 +2,7 @@ const handlerElephants = require('../src/handlerElephants');
 
 describe('Testes da função HandlerElephants', () => {
   it('ao passar o argumento count como parâmetro, deve retornar a quantidade de elefantes', () => {
-    const actual = handlerElephants('count');
-    const expected = 4;
-    expect(actual).toEqual(expected);
+    expect(handlerElephants('count')).toBe(4);
   });
   it('ao passar o argumento names deve retornar um array de nomes que possui o nome Jeffersons', () => {
     const actual = handlerElephants('names');
@@ -12,19 +10,13 @@ describe('Testes da função HandlerElephants', () => {
     expect(actual).toEqual(expected);
   });
   it('ao passar o argumento averageAge deve retornar um número próximo a 10.5', () => {
-    const actual = handlerElephants('averageAge');
-    const expected = 10.5;
-    expect(actual).toBeCloseTo(expected);
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
   });
   it('ao passar o argumento location deve retornar a localização dos elefantes dentro do Zoológico', () => {
-    const actual = handlerElephants('location');
-    const expected = 'NW';
-    expect(actual).toEqual(expected);
+    expect(handlerElephants('location')).toEqual('NW');
   });
   it('ao passar o argumento popularity deve retornar a popularidade dos elefantes, equivalente a um número igual ou maior a 5', () => {
-    const actual = handlerElephants('popularity');
-    const expected = 5;
-    expect(actual).toBeGreaterThanOrEqual(expected);
+    expect(handlerElephants('popularity')).toBeGreaterThanOrEqual(5);
   });
   it('ao passar o argumento availability deve retornar um array de dias da semana que não contém Monday', () => {
     const actual = handlerElephants('availability');
@@ -32,9 +24,12 @@ describe('Testes da função HandlerElephants', () => {
     expect(actual).toEqual(expected);
   });
   it('Não passando argumentos a função deve retornar undefined', () => {
-    // const actual = handlerElephants();
-    // const expected = undefined;
-    // expect(actual).toEqual(expected);
     expect(handlerElephants()).toBeUndefined();
+  });
+  it('ao passar como argumento um objeto vazio ({}), deve retornar a string "Parâmetro inválido, é necessário uma string"', () => {
+    expect(handlerElephants({})).toEqual('Parâmetro inválido, é necessário uma string');
+  });
+  it('ao passar como parâmetro uma string que não contempla uma funcionalidade deve retornar null', () => {
+    expect(handlerElephants('gatinhos')).toBeNull();
   });
 });
